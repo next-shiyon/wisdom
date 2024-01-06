3bf98e804731882c03cd4c1573f172c17be37799
next-shiyon <4.man.ach@gmail.com>
2024-01-06 19:46:10 +0900

    ✨ feat page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import HabitCreatePage from "./pages/habit/HabitCreatePage";
+import HistoryPage from "./pages/history/HistoryPage";
+import HistoryCreatePage from "./pages/history/HistoryCreatePage";
+import Layout from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <MainPage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/main", element: <MainPage /> },
+      { path: "/habit/create", element: <HabitCreatePage /> },
+      { path: "/history", element: <HistoryPage /> },
+      { path: "/history/create", element: <HistoryCreatePage /> },
+    ],
+  },
+]);
+
 export default function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
