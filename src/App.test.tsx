6ed38e80f44f25098f22b733b4bb60eab0f2ec8a
next-shiyon <4.man.ach@gmail.com>
2024-01-07 21:40:23 +0900

    🚚 move test files from __tests__ folder

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import App from "../../App";
+import App from "./App";
 
 describe("Application page routing", () => {
   test("Should routes Main page", async () => {
