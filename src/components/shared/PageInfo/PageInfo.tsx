0ceccfc171199f9424767d9dcb304fe57da5c276
next-shiyon <4.man.ach@gmail.com>
2024-01-10 23:07:33 +0900

    ✨ feat PageInfo component with test

---
+++ src/components/shared/PageInfo/PageInfo.tsx
@@ -0,0 +1,19 @@
+import {
+  StyledDesc,
+  StyledTitle,
+  TitleContainer,
+} from '../../../styles/PageInfo.styled';
+
+export type PageInfoProps = {
+  title: string;
+  description: string;
+};
+
+export default function PageInfo({ title, description }: PageInfoProps) {
+  return (
+    <TitleContainer>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledDesc>{description}</StyledDesc>
+    </TitleContainer>
+  );
+}
---
+++ src/styles/PageInfo.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const TitleContainer = styled.div`
+  padding: 32px;
+`;
+
+export const StyledTitle = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 0.625rem;
+`;
+
+export const StyledDesc = styled.span`
+  font-size: 1.125rem;
+`;
