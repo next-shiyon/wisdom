c35aa630ae47553801d837007bf1bee455bb1344
next-shiyon <4.man.ach@gmail.com>
2024-01-11 23:24:59 +0900

    ♻️ create LabelContainer for commonization

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,24 +1,4 @@
-import styled from 'styled-components';
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  > label {
-    text-transform: uppercase;
-    font-weight: 300;
-    font-size: 0.945rem;
-    margin-bottom: 8px;
-    color: hsl(0, 0%, 60%);
-  }
-
-  > input {
-    box-shadow: 0px 6px 13px -3px rgba(0, 0, 0, 0.1);
-    border-radius: 4px;
-    border: 1px solid hsl(0, 0%, 90%);
-    height: 40px;
-  }
-`;
+import { LabelContainer } from '../../styles/LabelContainer.styled';
 
 export type InputProps = {
   type: string;
@@ -27,9 +7,9 @@ export type InputProps = {
 
 export const Input = ({ type, label }: InputProps) => {
   return (
-    <InputContainer>
+    <LabelContainer>
       <label data-testid="input-label">{label}</label>
       <input data-testid="input" type={type} />
-    </InputContainer>
+    </LabelContainer>
   );
 };
